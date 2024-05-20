cee02871649bc5015d6e2dafca3dbc9b1fede3ce
petertle22 <petertle@outlook.com>
2024-05-20 09:54:51 -0400

    Created experience module and component

--- PersonalWebsite/src/app/app.routes.ts
+++ PersonalWebsite/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'experience',
+        loadChildren: () => 
+            import('./modules/experience/experience.module').then(
+                (m) => m.ExperienceModule
+            ),
+    }
+];
